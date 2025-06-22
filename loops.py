cb27a3205dd2b800a38b2bff1dcd4095b03c1ca2
adnanfrd <adnanmadhar74@gmail.com>
2025-06-22 19:13:26 +0500

    loops

---
+++ loops.py
@@ -0,0 +1,13 @@
+# Program 1: Print 1 to 5 using for loop
+for i in range(1, 6):
+    print(i)
+
+for j in range(0, 4) : 
+    print(j)  
+
+
+# Program 2: Countdown using while loop
+count = 5
+while count > 0:
+    print(count)
+    count -= 1
