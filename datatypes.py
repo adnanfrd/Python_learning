9ade8662f28b0c27ddf73e64686d49e535ef8a16
adnanfrd <adnanmadhar74@gmail.com>
2025-06-22 19:07:09 +0500

    data types

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+# Program 1: Use of int, float, str, and bool
+age = 25          # int
+height = 5.9      # float
+name = "Adnan Fareed"    # str
+is_online = True  # bool
+print(age, height, name, is_online)
+
+
+# Program 2: list, dict, tuple, set
+colors = ["red", "blue", "green"]
+person = {"name": "Ali", "age": 30}
+coordinates = (10, 20)
+unique_nums = {1, 2, 3, 1}
+print(colors, person, coordinates, unique_nums)
