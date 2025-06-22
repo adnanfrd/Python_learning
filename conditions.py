7a2d146448f9ca6f3d0c7aca6ef972ae8682be94
adnanfrd <adnanmadhar74@gmail.com>
2025-06-22 19:10:18 +0500

    conditions

---
+++ conditions.py
@@ -0,0 +1,16 @@
+# Program 1: Check even or odd
+num = 7
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Program 2: Grade checker
+marks = 92
+if marks >= 90:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+else:
+    print("Grade C")    
