71202d57aa821ed3c5b7b08145850d7d6716473c
adnanfrd <adnanmadhar74@gmail.com>
2025-06-22 19:04:24 +0500

    variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Program 1: Basic variable assignment
+x = 10
+y = 20
+print("Sum is:", x + y)
+
+# Program 2: Swapping variables
+a = 5
+b = 8
+a, b = b, a
+print("After swap: a =", a, ", b =", b)
